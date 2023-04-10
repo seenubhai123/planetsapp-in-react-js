88fe323a98c76c204fcc81674d36550055252c4f
seenubhai123 <thattesreenivasulu7876@gmail.com>
2023-04-10 04:33:08 +0000

    planets app in react js

---
+++ src/components/PlanetItem/index.js
@@ -0,0 +1,17 @@
+// Write your code here
+import React from 'react'
+import './index.css'
+
+const PlanetItem = props => {
+  const {details} = props
+  const {name, imageUrl, description} = details
+
+  return (
+    <div className="cardItem">
+      <img width="200" src={imageUrl} alt={`planet ${name}`} />
+      <h1>{name}</h1>
+      <p>{description}</p>
+    </div>
+  )
+}
+export default PlanetItem
